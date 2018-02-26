be51a6a4a08a420e639bf1e16d7dd7e6bdf81a0f
Cristian Privat <cristianprivat@gmail.com>
2018-02-26 22:24:54 +0100

    Fixing sombe module issues.

---
+++ andersenmod.f90
@@ -0,0 +1,25 @@
+module andersenmod
+use normaldist 
+   contains
+        subroutine Andersen(N_atoms,A_Prob,Velocities_mat,Tinst)
+                implicit none
+                real, dimension(N_atoms,3), intent(inout) :: Velocities_mat
+                integer, intent(in) :: N_atoms
+                real, intent(in) :: Tinst, A_Prob
+                real, dimension(3) :: probvec
+                integer :: ii
+                real :: random, sigma23
+
+                if (A_prob > 0) then
+                !Forzamos que nuestro sistema tenga Tinst.
+                sigma23 = sqrt(Tinst)
+                do ii=1, N_atoms !Hacemos un loop sobre todas las partículas
+                        Random = real(grnd())
+                        if (random .lt. A_Prob) then
+                                call r4vec_normal_ab(3,0.,Sigma23,probvec)
+                                Velocities_mat(ii,:) = probvec
+                        end if
+                end do
+                end if
+        end subroutine
+end module andersenmod
--- velocity_verlet.f90
+++ velocity_verlet.f90
@@ -1,10 +1,10 @@
-module velocity_verlet
-   implicit none
-
+module velocity_verletmod
+   use pbcmod
+   use LJ_forcemod
    contains
         ! Realizado -> Cristian !
         ! Algoritmo para Velocity Verlet !
-        subroutine velocity_verlet(N,dt,PosMat,VelMat,ForceMat,Pot_En)
+        subroutine velocity_verlet(N,dt,PosMat,VelMat,ForceMat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
                 implicit none
                 integer :: ii
                 integer, intent(in) :: N
@@ -12,18 +12,18 @@ module velocity_verlet
                 real, intent(out) :: Pot_En
                 real, dimension(N,3) :: NewForceMat
                 real, dimension(N,3), intent(inout) :: PosMat, VelMat, ForceMat
+                real, intent(in) :: Cutoff, C_U, Pressure, L_Intend
 
                 do ii = 1, N
                         PosMat(ii,:) = PosMat(ii,:) + VelMat(ii,:)*dt + 0.5*ForceMat(ii,:)*dt**2
                 end do
 
                 call pbc(PosMat,N,L_Intend)
-                call LJ_force(N_atoms,PosMat,NewForceMat,Pot_En)
+                call LJ_force(N,PosMat,NewForceMat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
 
                 do ii = 1, N
                         VelMat(ii,:) = VelMat(ii,:) + 0.5*(ForceMat(ii,:) + NewForceMat(ii,:))*dt
                         ForceMat(ii,:) = NewForceMat(ii,:)
                 end do
         end subroutine
-
-end module velocity_verlet
+end module velocity_verletmod
