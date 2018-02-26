9d02879c9ab9498930fc56cb29ec61a0a3903299
zazo100 <zazo100@hotmail.com>
2018-02-26 16:41:15 +0100

    Small changes in test data_visualisation to include legend

--- data_visualisation.py
+++ data_visualisation.py
@@ -5,6 +5,7 @@ import numpy as np
 
 time = []
 Ptot = []
+Press= []
 Epot = []
 Ekin = []
 Etot = []
@@ -16,17 +17,22 @@ with open("temp_res.xyz", 'r') as data:
             words = [float(words[i]) for i in range(len(words))]
             time.append(words[0])
             Ptot.append(words[1])
-            Epot.append(words[2])
-            Ekin.append(words[3])
-            Etot.append(words[4])
+            Press.append(words[2])
+            Epot.append(words[3])
+            Ekin.append(words[4])
+            Etot.append(words[5])
             
         except:
             pass
 
 x_list = time
 
-plt.plot(x_list, Epot)
-plt.title('test visualisation of data')
+plt.plot(x_list, Etot, label="Total Energy")
+plt.plot(x_list, Ekin, label="Kinectic Energy")
+plt.plot(x_list, Epot, label="Potential Energy")
+plt.legend()
+
+plt.title('Test visualisation of Energies')
 plt.xlabel('time (unit?)')
 plt.ylabel('Energy (au)')
 plt.show()
