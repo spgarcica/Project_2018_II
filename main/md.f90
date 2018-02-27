0829a95bafdf1576b42f6879110cfee823c1b465
ErikaFerreira <36412462+ErikaFerreira@users.noreply.github.com>
2018-02-27 09:53:05 +0100

    Comments translated
    I deleted the line 37 because there is no command there that is putting the matrix on the center.
    If Zazo in performing the binning of the data in his python program, is it necessary to have the binning in the main program?

--- main/md.f90
+++ main/md.f90
@@ -22,52 +22,55 @@ program MMDyn
         real, dimension(2) :: Time
         character(3) :: Integrator
 
-        ! Llamamos al reloj para saber el tiempo que tardaremos !
+        ! Initialization of the clock !
         call cpu_time(Time(1))
         print *, 'Reading parameters...'
-        ! Leemos el archivo de parámetros !
+        ! Reading the parameters file !
         call Read_file('param.dat')
-        ! Inicializamos el generador de número aleatorios !
+        ! Initializing the aleatory numbers generator !
         call sgrnd(Seed)
         print *, 'Generating geometry...'
-        ! Generamos la geometría con los parámetros leidos !
+        ! Generation of the geometry with the given parameters !
         call GenGeom(M,Density,CO_R,Position_mat,N_atoms,A_box,L_Intend,Velocity_mat,Force_Mat,Cutoff)
-        ! Generamos una función para reparar el cutoff !
+        ! Generation of the function to repair the Cutoff !
         call Repair_Cutoff(Cutoff,C_U)
-        !Ponemos la matriz en el centro, cambio de sistema de referencia útil al aplicar PBC!
         print *, 'Initializing velocities...'
-        ! Generamos las velocidades iniciales al azar !
+        ! Generation of random initial velocities !
         call Initial_Velocity(N_atoms,Sigma,Velocity_mat)
-        ! Generamos la primera matriz de fuerza !
+        ! Generation of the inital matrix of forces !
         call LJ_force(N_atoms,Position_mat,Force_Mat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
-        ! Llamamos a la función de la dinámica !
+        ! Calling the function that performs the dynamics !
         call Reaper(N_atoms,NSteps,NSta,Position_mat,Velocity_mat,Force_Mat,Integrator)
-        ! Volvemos a mirar el reloj !
+        ! Extracting the time taken to perform the dynamics !
         call cpu_time(Time(2))
         print *, 'All work done in:', Time(2)-Time(1), 's'
  
-        contains 
-        ! Realizado -> Sergio !
-        !Subrutina donde se especifica al programa lo que se desea hacer y se le aportan los parámetros necesarios!
+        contains
+        !---------------------------------------------------------------------------------------------------------!
+        !                                     Subroutine to read the parameters                                   !
+        ! Every parameter is given in reduced units and convert at the end of the program the give the results in !
+        ! the wanted units.                                                                                       !
+        ! If the cutoff ratio is higher than 0.5, the value won't be accepted due to a problem with high cutoffs  !
+        !---------------------------------------------------------------------------------------------------------!
         subroutine Read_file(filename)
                 implicit none
                 character(9), intent(in) :: filename
 
                 open(unit=25, file=filename, form='formatted', status='old')
                 read(25,*)
-                read(25,*) M, Density, CO_R, dt, Seed, Sigma, Temp !Se aporta todo en unidades reducidas!
-                ! Error si Cutoff muy grande !
+                read(25,*) M, Density, CO_R, dt, Seed, Sigma, Temp
+                ! Warning for cutoffs higher than 0.5 !
                 if (0.5 < CO_R) then
                         print *, 'INPUT ERROR: Invalid cutoff ratio, cutoff ratio must be smaller than 0.5'
                         STOP
                 end if
                 read(25,*) 
                 read(25,*) 
-                read(25,*) NSteps, NSta, S_Constant, E_Constant, M_Constant, A_Prob !Se corrigen las unidades después !
+                read(25,*) NSteps, NSta, S_Constant, E_Constant, M_Constant, A_Prob
                 read(25,*)
                 read(25,*)
                 read(25,*) Integrator, dr
-                ! Error si integrador no válido !
+                ! Warning if the integrator given is not valid !
                 if (Integrator /= 'Ver' .and. Integrator /= 'Eul') then
                         print *, 'INPUT ERROR: Integrator must be Ver o Eul'
                         STOP
@@ -75,16 +78,15 @@ program MMDyn
                 close(25)
         end subroutine
 
-        ! Realizado -> Sergio, Revisado -> Todos !
-        ! Esta es la subrutina principal, se encarga de realizar la dinámica con los datos    !
-        ! introducidos y de guardar los archivos, también corrije las unidades y calcula g(r) !
+        !---------------------------------------------------------------------------------------------!
+        !                         Subroutine that performs the molecular dynamic                      !
+        !---------------------------------------------------------------------------------------------!
         subroutine Reaper(N_atoms,NSteps,NSta,Position_mat,Velocity_mat,Force_Mat,Integrator)
                 implicit none
                 integer, intent(in) :: N_atoms, NSteps, NSta
                 character(3), intent(in) :: Integrator
                 real, dimension(N_atoms,3), intent(inout) :: Position_mat, Velocity_mat, Force_Mat
                 integer :: Counter1, Counter2, Counter3, NHist
-                integer(8) :: HistCount
                 real :: Pot_En, Temperature
                 real, dimension(NSteps) :: Pressure_Mat, Temperature_Mat, KE_Mat, PE_Mat
                 real, dimension(3) :: Vel_Aver, SD_Value
@@ -95,7 +97,7 @@ program MMDyn
                 Temperature = 0
                 Vel_Aver = 0.
 
-                ! Apuntamos a las subrutinas según integrador !
+                ! Pointer to the subroutine with the choosen integrator !
                 select case(Integrator)
                 case ('Eul')
                         Int_Type => Euler
@@ -105,37 +107,36 @@ program MMDyn
 
                 print *, 'Calculating initialization steps...'
                 do Counter1=1, NSta
-                        ! Esto escribe una barra de progreso, usar -O3 al compilar !
+                        ! Writes a progression bar. Use -O3 option when compiling !
                         write(*,FMT="(A1,A,t21,F6.2,A)",ADVANCE="NO") achar(13), &
                            " Percent Complete: ", (real(Counter1)/real(NSta))*100.0, "%"
 
-                        ! Primer Andersen con 0.1 de probabilidad fija para estabilizar el sistema !
+                        ! First Andersen with fixed 0.1 probability to stabilize the system !
                         call Andersen(N_atoms,0.1,Velocity_mat,Temp)
                         call Int_Type(N_atoms,dt,Position_mat,Velocity_mat,Force_Mat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
                 end do
                 print *, ''
 
-                ! Corregimos el momento a 0 después del primer Andersen de estabilización !
+                ! Resetting the momentum after the stabilization Andersen !
                 do Counter1=1, 3
                         Velocity_mat(:,Counter1) = Velocity_mat(:,Counter1) - sum(Velocity_mat(:,Counter1))/N_atoms
                 end do
 
-                ! Abrimos los archivos dónde volvamos los datos !
+                ! Opening of the files where the final data will be outputted !
                 open(unit=25, file='traj.xyz', form='formatted', status='unknown')
                 open(unit=26, file='ener.xyz', form='formatted', status='unknown')
                 open(unit=27, file='testvel', form='formatted', status='unknown')
                 write(26,FMT="(A14,A17,A17,A17,A17,A17)") &
                         'Time', 'Total momentum', 'Pressure', 'Potential Enenergy', 'Kin Energy', 'Total Energy'
 
-                ! Inizializamos g(r) !
+                ! Initializing the g(r) !
                 NHist = int(L_intend/dr)
-                HistCount = 0.
                 allocate(HistProfile(NHist+1))
                 HistProfile = 0.
 
                 print *, 'Calculating the dynamic...'
                 do Counter1=1, NSteps
-                        ! Esto escribe una barra de progreso, usar -O3 al compilat !
+                        ! Writes a progression bar. Use -O3 option when compiling !
                         write(*,FMT="(A1,A,t21,F6.2,A)",ADVANCE="NO") achar(13), &
                            " Percent Complete: ", (real(Counter1)/real(NSteps))*100.0, "%"
 
@@ -150,7 +151,7 @@ program MMDyn
 
                         call rdf(Position_mat,N_atoms,dr,NHist,L_intend,Counter1,Nsteps,Density,HistProfile,HistCount)
 
-                        ! Corregimos unidades y guardamos !
+                        ! Correcting the units and saving the results !
                         Pressure_Mat(Counter1) = (Pressure/(3*L_Intend**3)+Density*Temp)*(E_Constant/(S_Constant**3))
                         KE_Mat(Counter1) = Kinetic_E(N_atoms,Velocity_mat)*E_Constant
                         PE_Mat(Counter1) = Pot_En*E_Constant
@@ -166,11 +167,11 @@ program MMDyn
                                 Vel_Aver = Vel_Aver + Velocity_mat(Counter3,:)*sqrt(E_constant/M_constant)
                         end do
                 end do
-                ! Generamos una gaussiana para después comparar con las velocidades !
+                ! Generation of a Gaussian curve !
                 call gauss(Temp*(E_constant/Boltz),N_atoms,NSteps)
                 print *, ''
                 
-                ! Imprimimos los datos con su desvest !
+                ! Printing all the results with their standard deviation !
                 print *, 'Average Temperature (Reduced units)'
                 print *,  Temperature/NSteps*Boltz/E_Constant, '+-', Sdeviation(NSteps,Temperature_Mat*Boltz/E_Constant,NSteps)
                 print *, 'Average Pressure'
@@ -186,9 +187,7 @@ program MMDyn
                 close(26)
                 close(27)
 
-                ! Realizamos binning para calcular mejor desvest  !
-                ! no funciona demasiado bien con valores pequeños !
-                ! de NSteps y NSta                                !
+                ! Binning the data to improve the standard deviation calculations !
                 call Binning(NSteps,Pressure_Mat,'pressu') 
                 call Binning(NSteps,Temperature_Mat,'temper') 
                 call Binning(NSteps,KE_Mat,'kineti') 
@@ -198,7 +197,7 @@ program MMDyn
                 print *, 'Calculating SD of velocities...'
                 call SD_Calculator(N_Atoms*NSteps,'testvel',Vel_Aver,SD_Value)
                 print *, 'Velocity average:', Vel_Aver
-                ! La desviación estándar es raiz de T !
+                ! The standard deviation is square root of T !
                 print *, 'Velocity Sdeviation:', SD_Value
         end subroutine
 end program
