974259c0584d27f208b745a1e08336e78b5b05fb
ErikaFerreira <36412462+ErikaFerreira@users.noreply.github.com>
2018-02-28 11:16:07 +0100

    Comments translated and binning deleted
    I deleted the binning, because we are not using it in the main, and I translated the comments.

--- math/statisticalsmod.f90
+++ math/statisticalsmod.f90
@@ -1,9 +1,11 @@
 module statisticalsmod
 use stdmod
 contains
-        ! Esta subrutina calcula la desviación estándar de la velocidad !
-        ! Para ello lee un archivo con los xyz de la velocidad y trata  !
-        ! los datos. Nótese que se necesita saber el promedio.          !
+        !----------------------------------------------------------------------------!
+        !     Subroutine that calculates the standard deviation of the velocities    !
+        ! It reads the velocities from a file and has to know the average of the     !
+        ! results listed in the file.                                                !
+        !----------------------------------------------------------------------------!
         subroutine SD_Calculator(N_Units,Filename,Average,SD_Value)
                 implicit none
                 integer, intent(in) :: N_Units
@@ -22,49 +24,5 @@ contains
                 end do
                 SD_Value = SD_Value * (1./(N_Units-1.))
                 SD_Value = sqrt(SD_Value)
-        end subroutine
-
-        ! Reciclado de otro programa !
-        ! Subrutina para hacer un vector de binning !
-        subroutine binsvector(nMC,nmeas,timenergyvec,bins,binvec)
-                implicit none
-                integer, intent(in) :: nmeas
-                integer, intent(in) :: nMC, bins 
-                real, dimension(nMC/nmeas), intent(in) :: timenergyvec
-                real, dimension((nMC/nmeas)/bins), intent(out) :: binvec
-                integer, dimension(2) :: binsplit
-                integer :: bini
-                integer :: newdimension
-
-                newdimension = (nMC/nmeas)/bins
-
-                do bini=1, newdimension
-                        binsplit(1) = ((bini-1)*bins)+1
-                        binsplit(2) = bini*bins
-                        binvec(bini) = sum(timenergyvec(binsplit(1):binsplit(2)))/(newdimension)
-                end do
-        end subroutine
-
-        ! Reciclado de otro programa !
-        ! Subrutina para realizar binning !
-        subroutine Binning(NSteps,Mat,Filename) 
-                implicit none
-                character(6), intent(in) :: Filename
-                integer, intent(in) :: NSteps
-                real, dimension(NSteps), intent(out) :: Mat
-                integer :: BinsN, newdimension
-                real, dimension(:), allocatable :: binvec
-
-                BinsN = 1.
-                open(unit=30, file=Filename, form='formatted', status='unknown')
-                do while (BinsN < NSteps/2)
-                        BinsN = BinsN*2
-                        newdimension=NSteps/BinsN
-                        allocate(binvec(newdimension))
-                        call binsvector(NSteps,1,Mat,BinsN,binvec)
-                        write(30,*) sdeviation(newdimension,binvec,Nsteps)
-                        deallocate(binvec)
-                end do
-                close(30)
         end subroutine
 end module statisticalsmod
