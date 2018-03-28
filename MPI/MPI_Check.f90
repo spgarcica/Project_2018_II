6dc90a78ed125e01e6bf19af7fc5793fca257df9
Sergio Pablo <spgarcica@gmail.com>
2018-03-28 15:08:32 +0200

    MPI subroutines

---
+++ MPI/MPI_Check.f90
@@ -0,0 +1,13 @@
+module MPI_Check
+        include 'mpif.h'
+        contains
+                subroutine Check(M,num_proc,myrank)
+                        integer, intent(in) :: M, num_proc, myrank
+                        if ((mod(M,num_proc) /= 0) .or. (M < num_proc)) then
+                                if (myrank == 0) then
+                                        print *, "System dimension isn't compatible with process number"
+                                end if
+                                STOP
+                        end if
+                end subroutine
+end module MPI_Check
---
+++ MPI/MPI_regroup.f90
@@ -0,0 +1,25 @@
+module MPI_Regroup
+        include 'mpif.h'
+        contains
+                subroutine Regroup(ierror,myrank,num_proc,N_Slice,N_Dimension,Matrix,R_Matrix)
+                        integer, intent(in) :: ierror, myrank, num_proc, N_Slice, N_Dimension
+                        real, dimension(N_Slice*num_proc,N_Dimension), intent(in) :: Matrix
+                        real, dimension(N_Slice*num_proc,N_Dimension), intent(out) :: R_Matrix
+                        real, dimension(N_Slice*num_proc*N_Dimension) :: Aux_Matrix
+                        integer :: rank, Counter, ii, jj
+
+                        do rank=0, num_proc-1
+                                if (myrank == rank) then
+                                        Aux_Matrix = reshape(Matrix,(/N_Dimension*N_Slice/))
+                                end if
+                                call MPI_BCAST(Aux_Matrix,N_Dimension*N_Slice,MPI_REAL,rank,MPI_COMM_WORLD,ierror)
+                                Counter = 1
+                                do ii=(rank*N_Slice)+1, (rank+1)*N_Slice
+                                        do jj=1, N_Dimension
+                                                R_Matrix(ii,jj) = Aux_Matrix(Counter+(N_Slice*(jj-1)))
+                                                Counter = Counter + 1
+                                        end do
+                                end do
+                        end do
+                end subroutine
+end module MPI_Regroup
