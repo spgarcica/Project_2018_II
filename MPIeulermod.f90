9b7db1ced20e8f267c5afcd4c04918c5a5282385
Cristian Privat <36412419+cprivat@users.noreply.github.com>
2018-03-28 10:27:15 +0000

    Correct call of PBC

--- MPIeulermod.f90
+++ MPIeulermod.f90
@@ -13,11 +13,12 @@ contains
                 partition = N_atoms/num_proc
                 jj = 0
                   
-                call pbc(Pos_mat,partition,L)
                 do ii = (partition*myrank)+1, (myrank+1)*partition
                           jj = jj + 1
                           Pos_mat(jj,:) = Pos_mat_total(ii,:) + Vel_mat_total(ii,:)*dt + 0.5*Force_mat_total(ii,:)*dt**2
                           Vel_mat(jj,:) = Vel_mat_total(ii,:) + Force_mat_total(ii,:)*dt
                 end do
+                call pbc(Pos_mat,partition,L)
+
          end subroutine
 end module eulermod
