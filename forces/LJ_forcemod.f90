1e917a7972b97f509e00c40b3ed72b15b3c7ab72
Sergio Pablo <spgarcica@gmail.com>
2018-03-29 03:02:10 +0200

    Brand new Parallel LJ

--- forces/LJ_forcemod.f90
+++ forces/LJ_forcemod.f90
@@ -5,13 +5,15 @@ contains
         !  Subroutine that provides a matrix with the forces that operate in each particle and the potential   ! 
         !  energy.                                                                                             !
         !------------------------------------------------------------------------------------------------------!
-        subroutine LJ_force(N_atoms,Position_mat,Force_Mat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
+        subroutine LJ_force(N_atoms,N_Interactions,myrank,num_proc,LJ_IntMat,Position_mat, &
+                        Force_Mat,Pot_En,Cutoff,C_U,Pressure,L_Intend)
                implicit none
-               integer, intent(in) :: N_atoms
+               integer, intent(in) :: N_atoms, N_Interactions, myrank, num_proc
+               integer, dimension(N_Interactions,2), intent(in) :: LJ_IntMat
                real, dimension(N_atoms,3), intent(in) :: Position_mat
                real, intent(out) :: Pot_En
                real, dimension(N_atoms,3), intent(out) :: Force_Mat
-               integer :: iii, jjj, kkk
+               integer :: iii, jjj, kkk, lll, Assigner, A_From, A_Until
                real :: Cutoff2, Distance_2, Distance_4, Distance_6, Distance_8, Distance_12, Distance_14, Dist_Aux
                real, dimension(3) :: Distance_vec, Force_vec
                real :: Cutoff, C_U, Pressure, L_Intend
@@ -20,34 +22,66 @@ contains
                Pot_En = 0.
                Cutoff2 = Cutoff**2
                Pressure = 0
+               Assigner = N_Interactions/num_proc
+               A_From = (myrank*Assigner)+1
+               A_Until = A_From + Assigner - 1
 
-               do iii=1, N_Atoms
-                       do jjj=iii+1, N_Atoms
-                               Distance_vec = Position_mat(iii,:)-Position_mat(jjj,:)
+               do lll=A_From, A_Until
+                       iii = LJ_IntMat(lll,1)
+                       jjj = LJ_IntMat(lll,2)
+                       Distance_vec = Position_mat(iii,:)-Position_mat(jjj,:)
+                       Distance_2 = Distance_vec(1) ** 2 + Distance_vec(2) ** 2 + Distance_vec(3) ** 2
+                       ! If the distance is too high, minumum image conditions are imposed !
+                       if (sqrt(Distance_2) > Cutoff) then
+                               do kkk=1, 3
+                                       Dist_Aux = Position_mat(iii,kkk) - Position_mat(jjj,kkk)
+                                       if (abs(Dist_Aux) > cutoff) then
+                                               Distance_vec(kkk) = Distance_vec(kkk)-sign(L_Intend,Dist_Aux)
+                                       end if
+                               end do
                                Distance_2 = Distance_vec(1) ** 2 + Distance_vec(2) ** 2 + Distance_vec(3) ** 2
-                               ! If the distance is too high, minumum image conditions are imposed !
-                               if (sqrt(Distance_2) > Cutoff) then
-                                       do kkk=1, 3
-                                               Dist_Aux = Position_mat(iii,kkk) - Position_mat(jjj,kkk)
-                                               if (abs(Dist_Aux) > cutoff) then
-                                                       Distance_vec(kkk) = Distance_vec(kkk)-sign(L_Intend,Dist_Aux)
-                                               end if
-                                       end do
-                                       Distance_2 = Distance_vec(1) ** 2 + Distance_vec(2) ** 2 + Distance_vec(3) ** 2
-                               end if
-                               Distance_4 = Distance_2**2
-                               Distance_6 = Distance_4*Distance_2
-                               Distance_8 = Distance_4**2
-                               Distance_12 = Distance_6**2
-                               Distance_14 = Distance_8*Distance_6
-                               if (Distance_2 < Cutoff2) then
-                                       Force_vec = (48./Distance_14 - 24./Distance_8) * (Distance_vec/Distance_2)
-                                       Force_Mat(iii,:) = Force_Mat(iii,:) + Force_vec
-                                       Force_Mat(jjj,:) = Force_Mat(jjj,:) - Force_vec
-                                       Pot_En = Pot_En + 4.*(1./Distance_12 - 1./Distance_6) - C_U
-                                       Pressure = Pressure + abs(Dot_Product(Force_vec,Distance_vec))
-                               end if
-                       end do
+                       end if
+                       Distance_4 = Distance_2**2
+                       Distance_6 = Distance_4*Distance_2
+                       Distance_8 = Distance_4**2
+                       Distance_12 = Distance_6**2
+                       Distance_14 = Distance_8*Distance_6
+                       if (Distance_2 < Cutoff2) then
+                               Force_vec = (48./Distance_14 - 24./Distance_8) * (Distance_vec/Distance_2)
+                               Force_Mat(iii,:) = Force_Mat(iii,:) + Force_vec
+                               Force_Mat(jjj,:) = Force_Mat(jjj,:) - Force_vec
+                               Pot_En = Pot_En + 4.*(1./Distance_12 - 1./Distance_6) - C_U
+                               Pressure = Pressure + abs(Dot_Product(Force_vec,Distance_vec))
+                       end if
                end do
+        end subroutine
+        
+        subroutine Interactions_Init(N_Atoms,LJ_IntMat,N_Interactions)
+                implicit none
+                integer, intent(in) :: N_Atoms
+                integer, dimension(:,:), allocatable, intent(out) :: LJ_IntMat
+                integer, intent(out) :: N_Interactions
+                integer :: Accumulator, ii, jj
+
+                Accumulator = 0
+
+                ! Calculating the number of pair interactions !
+                do ii=1, N_Atoms-1
+                        Accumulator = Accumulator + ii
+                end do
+
+                N_Interactions = Accumulator
+
+                allocate(LJ_IntMat(Accumulator,2))
+
+                Accumulator = 1
+               
+                do ii=1, N_Atoms
+                        do jj=ii+1, N_Atoms
+                                LJ_IntMat(Accumulator,1) = ii
+                                LJ_IntMat(Accumulator,2) = jj
+                                Accumulator = Accumulator + 1
+                        end do
+                end do
         end subroutine
 end module LJ_forcemod
